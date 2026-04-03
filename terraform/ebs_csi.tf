2e97d8068101dd7445f57460aaf261b6dc1fc3d3
jounghyeon <jounghyeon123@gmail.com>
2026-04-03 15:12:14 +0900

    ./gitignore에서 secret.yaml이 추가되고 있는 것을 제거

---
+++ terraform/ebs_csi.tf
@@ -0,0 +1,50 @@
+# EBS CSI Driver IRSA Role
+# EBS CSI Controller가 EBS 볼륨을 생성/삭제할 수 있는 IAM 권한
+# KS 1.32에서 kubernetes.io/aws-ebs (in-tree) → 이미 deprecated, 동작 안 함
+# 해결책은 버전 변경이 아니라 EBS CSI 드라이버 addon 설치
+
+data "aws_iam_policy_document" "ebs_csi_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ebs_csi" {
+  name               = "${var.project_name}-ebs-csi-role"
+  assume_role_policy = data.aws_iam_policy_document.ebs_csi_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi.name
+}
+
+# EBS CSI Driver EKS Addon
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = aws_eks_cluster.main.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi.arn
+
+  tags = {
+    Name = "${var.project_name}-ebs-csi"
+  }
+}

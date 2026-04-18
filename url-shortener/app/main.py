f035fba6ef2e04f74785f0a8fb7fff60a4ead2de
jounghyeon <jounghyeon123@gmail.com>
2026-04-18 14:55:52 +0900

    불필요한 import 수정

--- url-shortener/app/main.py
+++ url-shortener/app/main.py
@@ -1,17 +1,14 @@
 import logging
-import time
-
-from contextlib import asynccontextmanager
-from app.api.routes import health, item
-from app.core.database import Base, write_engine
-
-logging.basicConfig(level=logging.INFO)
-
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
 from prometheus_client import make_asgi_app
+
+from app.api.routes import health, item
+from app.core.database import Base, write_engine
 from app.core.metrics import http_request_total
+
+logging.basicConfig(level=logging.INFO)
 
 
 @asynccontextmanager  # 비동기 context manager를 만들기 위한 데코레이터
@@ -21,7 +18,6 @@ async def lifespan(app: FastAPI):
     DB 테이블 생성을 여기서 수행해 DB 준비 후 실행을 보장.
     """
     # 시작 시: DB 테이블 생성 (이미 있으면 건너뜀)
-    # Base.metadata.create_all(bind=engine)
     Base.metadata.create_all(bind=write_engine)  # Primary에 테이블 생성
     yield
     # 종료 시: 필요한 정리 작업 추가 가능

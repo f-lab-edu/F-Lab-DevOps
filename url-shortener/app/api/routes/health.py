b15b693f64813dce90ecb257cbf7a8fb4da2ec97
jounghyeon <jounghyeon123@gmail.com>
2026-08-14 11:58:14 +0900

    잘못된 traefik 문법 수정 및 version 수정

--- url-shortener/app/api/routes/health.py
+++ url-shortener/app/api/routes/health.py
@@ -8,7 +8,7 @@ def healthcheck():
     """
     서버 상태를 확인하는 헬스체크 엔드포인트.
     """
-    return {"status": "ok", "version": "v34"}
+    return {"status": "ok", "version": "v35"}
 
 
 @router.get("/error-test")

a5ffbd06151885fd35180f5b1cd9337252d6e784
jounghyeon <jounghyeon123@gmail.com>
2026-09-23 14:04:24 +0900

    fix(ci): Terraform 포맷 오류와 Node.js 20 Action 경고 수정
    - Terraform fmt 검사에서 실패한 파일의 서식 정리
    - GitHub Actions를 Node.js 24 지원 버전의 commit SHA로 갱신
    - 의존성 검사 워크플로의 YAML 명령 구문 수정

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,14 +2,14 @@
 
 variable "aws_region" {
   description = "AWS 리전"
-  type = string
-  default = "ap-northeast-2"
+  type        = string
+  default     = "ap-northeast-2"
 }
 
 variable "project_name" {
   description = "프로젝트 이름 - 리소스의 prefix로 사용"
-  type = string
-  default = "urlshortener"
+  type        = string
+  default     = "urlshortener"
 }
 
 # 실습 환경은 운영과 별도 state 및 project_name을 사용한다.
@@ -26,47 +26,47 @@ variable "environment" {
 
 variable "vpc_cidr" {
   description = "VPC CIDR 블록"
-  type = string
-  default = "10.0.0.0/16"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
   description = "사용할 가용 영역 목록"
-  type = list(string)
-  default = ["ap-northeast-2a", "ap-northeast-2c"]
+  type        = list(string)
+  default     = ["ap-northeast-2a", "ap-northeast-2c"]
 }
 
 variable "eks_cluster_version" {
   description = "EKS Kubernetes 버전"
-  type = string
-  default = "1.35"
+  type        = string
+  default     = "1.35"
 }
 
 variable "node_instance_type" {
   description = "EKS Worker Node EC2 인스턴스 타입"
-  type = string
-  default = "t3.large"
+  type        = string
+  default     = "t3.large"
 }
 
 # terraform apply 시 생성되는 노드 수
 variable "node_desired_size" {
   description = "NodeGroup 기본 노드 수"
-  type = number
-  default = 2
+  type        = number
+  default     = 2
 }
 
 # 스케일 다운 시 보장되는 최소 노드 수
 variable "node_min_size" {
   description = "NodeGroup 최소 노드 수"
-  type = number
-  default = 1
+  type        = number
+  default     = 1
 }
 
 # 스케일 업 시 보장되는 최대 노드 수
 variable "node_max_size" {
   description = "NodeGroup 최대 노드 수"
-  type = number
-  default = 4
+  type        = number
+  default     = 4
 }
 
 variable "db_password" {
